004b6e093dfdeab8e17e73ad323917f745752353
rodrigo.goncalves <ro.go.calves@gmail.com>
2025-05-22 22:26:32 -0300

    refactor(LeRechum): Mover e Traduzir settings

--- instituto_langchain/le_rec_hum/settings.py
+++ instituto_langchain/le_rec_hum/settings.py
@@ -8,21 +8,22 @@ handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 LOGGER.addHandler(handler)
-LOGGER.propagate = False  # Evita mensagens duplicadas no logger raiz
+LOGGER.propagate = False  # Prevents duplicate messages in the root logger
 
 
-class Configuracoes(BaseSettings):
+class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", extra="ignore", env_file_encoding="utf-8"
     )
-    
-    GROQ_API_KEY: str
-    
-    TTQ_NOME_MODELO: str = "deepseek-r1-distill-llama-70b"
-    
-    CAMINHO_BANCO_AGENTE_SQL: str = ".db/AGENTE_SQL.db"
 
-    # Configuração do logger
+    GROQ_API_KEY: str
+
+    TTQ_MODEL_NAME: str = "deepseek-r1-distill-llama-70b"
+
+    AGENT_SQL_DB_PATH: str = ".db/AGENT_SQL.db"
+
+    # Logger configuration
     LOGGER: ClassVar[logging.Logger] = LOGGER
-    
-configuracoes = Configuracoes()
+
+
+settings = Settings()
